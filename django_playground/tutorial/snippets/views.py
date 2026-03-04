f06e0ae2b9d03cb442b36a9003e37eb4da8bd207
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-03-04 20:44:50 +0200

    Remove `api_root` view from views.py as it is no longer used

--- django_playground/tutorial/snippets/views.py
+++ django_playground/tutorial/snippets/views.py
@@ -9,16 +9,6 @@ from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
 
-@api_view(["GET"])
-def api_root(request, format=None):
-    return Response(
-        {
-            "users": reverse("user-list", request=request, format=format),
-            "snippets": reverse("snippet-list", request=request, format=format),
-        }
-    )
-
-
 class SnippetViewSet(viewsets.ModelViewSet):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
