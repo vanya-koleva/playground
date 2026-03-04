38cbd31223c1caa8f3e94856b03cdcad21d5db9e
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-03-04 20:13:02 +0200

    Switch to `HyperlinkedModelSerializer` and add URL names to snippet and user paths

--- django_playground/tutorial/snippets/serializers.py
+++ django_playground/tutorial/snippets/serializers.py
@@ -2,20 +2,35 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from snippets.models import Snippet
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name="snippet-highlight",
+        format="html"
+    )
 
     class Meta:
         model = Snippet
-        fields = ["id", "title", "code", "linenos", "language", "style", "owner"]
+        fields = [
+            "url",
+            "id",
+            "highlight",
+            "title",
+            "code",
+            "linenos",
+            "language",
+            "style",
+            "owner"
+        ]
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
         many=True,
-        queryset=Snippet.objects.all()
+        view_name="snippet-detail",
+        read_only=True
     )
 
     class Meta:
         model = User
-        fields = ["id", "username", "snippets"]
+        fields = ["url", "id", "username", "snippets"]
