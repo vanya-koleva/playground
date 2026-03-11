d9bc9dcea6256727ec1ec65e98d2f8806dec2faa
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-03-11 17:56:41 +0200

    Customize admin interface

--- django_playground/mdn_django_tutorial/catalog/admin.py
+++ django_playground/mdn_django_tutorial/catalog/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Book, Genre, Author, BookInstance, Language
+
+# admin.site.register(Book)
+# admin.site.register(Author)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
+admin.site.register(Genre)
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "display_genre"     # ManyToManyField -> large database access "cost"
+    )
+    inlines = [BookInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ("book", "status", "due_back", "id")
+    list_filter = ("status", "due_back")
+    fieldsets = (
+        (None, {
+            "fields": ("book", "imprint", "id")
+        }),
+        ("Availability", {
+            "fields": ("status", "due_back")
+        }),
+    )
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        "last_name",
+        "first_name",
+        "date_of_birth",
+        "date_of_death"
+    )
+    fields = [
+        "first_name",
+        "last_name",
+        ("date_of_birth", "date_of_death")
+    ]
+    inlines = [BooksInline]
+
+admin.site.register(Author, AuthorAdmin)
