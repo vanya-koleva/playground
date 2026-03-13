2fd53c0cbdbf260b669f8e88d2249e717143b9d3
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-03-13 16:03:00 +0200

    Create list and detail pages for books and authors

--- django_playground/mdn_django_tutorial/catalog/views.py
+++ django_playground/mdn_django_tutorial/catalog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Author, Book, BookInstance, Genre
 
 
@@ -16,3 +18,28 @@ def index(request):
     }
 
     return render(request, "index.html", context=context)
+
+
+class BookListView(ListView):
+    model = Book
+    paginate_by = 10
+
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+
+
+class BookDetailView(DetailView):
+    model = Book
+
+
+class AuthorListView(ListView):
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(DetailView):
+    model = Author
