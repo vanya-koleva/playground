172ab683cc6bfbdc4011878996cae7e247eafa1c
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2026-03-14 12:30:16 +0200

    Add visit counts

--- django_playground/mdn_django_tutorial/catalog/views.py
+++ django_playground/mdn_django_tutorial/catalog/views.py
@@ -10,11 +10,17 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact="a").count()
     num_authors = Author.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get("num_visits", 0)
+    num_visits += 1
+    request.session["num_visits"] = num_visits
+
     context = {
         "num_books": num_books,
         "num_instances": num_instances,
         "num_instances_available": num_instances_available,
         "num_authors": num_authors,
+        "num_visits": num_visits,
     }
 
     return render(request, "index.html", context=context)
